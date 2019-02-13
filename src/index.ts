1df5d9895306298bb0b900ad53b616989e30c229
Kobkrit Viriyayudhakorn <kobkrit@gmail.com>
2019-02-13 12:58:30 +0700

    Add Typeform

--- src/index.ts
+++ src/index.ts
@@ -1,173 +1,178 @@
-require('dotenv').config()
+require("dotenv").config();
 
-import axios from 'axios'
-import bodyParser from 'body-parser'
-import express from 'express'
-import { DiscordPayload } from './model/DiscordPayload'
-import { BaseProvider } from './provider/BaseProvider'
-import { ErrorUtil } from './util/ErrorUtil'
-import { LoggerUtil } from './util/LoggerUtil'
+import axios from "axios";
+import bodyParser from "body-parser";
+import express from "express";
+import { DiscordPayload } from "./model/DiscordPayload";
+import { BaseProvider } from "./provider/BaseProvider";
+import { ErrorUtil } from "./util/ErrorUtil";
+import { LoggerUtil } from "./util/LoggerUtil";
 
-import { AppVeyor } from './provider/Appveyor'
-import { Bintray } from './provider/Bintray'
-import { BitBucket } from './provider/Bitbucket'
-import { CircleCi } from './provider/CircleCi'
-import { Codacy } from './provider/Codacy'
-import { DockerHub } from './provider/DockerHub'
-import { GitLab } from './provider/GitLab'
-import { Heroku } from './provider/Heroku'
-import { Jenkins } from './provider/Jenkins'
-import { Jira } from './provider/Jira'
-import { NewRelic } from './provider/NewRelic'
-import { Patreon } from './provider/Patreon'
-import { Pingdom } from './provider/Pingdom'
-import { Travis } from './provider/Travis'
-import { Trello } from './provider/Trello'
-import { Unity } from './provider/Unity'
-import { VSTS } from './provider/VSTS'
+import { AppVeyor } from "./provider/Appveyor";
+import { Bintray } from "./provider/Bintray";
+import { BitBucket } from "./provider/Bitbucket";
+import { CircleCi } from "./provider/CircleCi";
+import { Codacy } from "./provider/Codacy";
+import { DockerHub } from "./provider/DockerHub";
+import { GitLab } from "./provider/GitLab";
+import { Heroku } from "./provider/Heroku";
+import { Jenkins } from "./provider/Jenkins";
+import { Jira } from "./provider/Jira";
+import { NewRelic } from "./provider/NewRelic";
+import { Patreon } from "./provider/Patreon";
+import { Pingdom } from "./provider/Pingdom";
+import { Travis } from "./provider/Travis";
+import { Trello } from "./provider/Trello";
+import { Typeform } from "./provider/Typeform";
+import { Unity } from "./provider/Unity";
+import { VSTS } from "./provider/VSTS";
 
-LoggerUtil.init()
+LoggerUtil.init();
 
-const logger = LoggerUtil.logger()
-logger.debug('Winston setup successfully.')
+const logger = LoggerUtil.logger();
+logger.debug("Winston setup successfully.");
 
-const app = express()
+const app = express();
 
 /**
  * Array of the classes
  */
 const providers: any[] = [
-    AppVeyor,
-    Bintray,
-    BitBucket,
-    CircleCi,
-    Codacy,
-    DockerHub,
-    GitLab,
-    Heroku,
-    Jenkins,
-    Jira,
-    NewRelic,
-    Patreon,
-    Pingdom,
-    Travis,
-    Trello,
-    Unity,
-    VSTS
-]
+  AppVeyor,
+  Bintray,
+  BitBucket,
+  CircleCi,
+  Codacy,
+  DockerHub,
+  GitLab,
+  Heroku,
+  Jenkins,
+  Jira,
+  NewRelic,
+  Patreon,
+  Pingdom,
+  Travis,
+  Trello,
+  Typeform,
+  Unity,
+  VSTS
+];
 
-const providersMap = new Map<string, any>()
-const providerNames: string[] = []
-const providerInstances: BaseProvider[] = []
-const providerInfos: any[] = []
-providers.forEach((Provider) => {
-    const instance = new Provider()
-    providerInstances.push(instance)
-    providersMap.set(instance.getPath(), Provider)
-    console.log(`Adding provider name ${instance.getName()}`)
-    providerNames.push(instance.getName())
-    const providerInfo = {
-        name: instance.getName(),
-        path: instance.getPath()
+const providersMap = new Map<string, any>();
+const providerNames: string[] = [];
+const providerInstances: BaseProvider[] = [];
+const providerInfos: any[] = [];
+providers.forEach(Provider => {
+  const instance = new Provider();
+  providerInstances.push(instance);
+  providersMap.set(instance.getPath(), Provider);
+  console.log(`Adding provider name ${instance.getName()}`);
+  providerNames.push(instance.getName());
+  const providerInfo = {
+    name: instance.getName(),
+    path: instance.getPath()
+  };
+  providerInfos.push(providerInfo);
+});
+providerNames.sort();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index", { providers: providerInfos });
+});
+
+app.get("/providers", (req, res) => {
+  res.status(200).send(providerNames);
+});
+
+app.get("/api/webhooks/:webhookID/:webhookSecret/:from", (req, res) => {
+  // Return 200 if the provider is valid to show this url is ready.
+  const provider: any = req.params.from;
+  if (provider == null || providersMap.get(provider) == null) {
+    logger.error(`Unknown provider: ${provider}`);
+    res.sendStatus(400);
+  } else {
+    res.sendStatus(200);
+  }
+});
+
+app.post("/api/webhooks/:webhookID/:webhookSecret/:from", async (req, res) => {
+  const webhookID = req.params.webhookID;
+  const webhookSecret = req.params.webhookSecret;
+  const providerName = req.params.from;
+  if (!webhookID || !webhookSecret || !providerName) {
+    res.sendStatus(400);
+    return;
+  }
+  const discordEndpoint =
+    "https://discordapp.com/api/webhooks/" + webhookID + "/" + webhookSecret;
+
+  let discordPayload: DiscordPayload = null;
+  const error = false;
+
+  const Provider = providersMap.get(providerName);
+  if (Provider != null) {
+    const instance: BaseProvider = new Provider();
+    try {
+      // seems dumb, but this is the best way I know how to format these headers in a way we can use them
+      const headersString = JSON.stringify(req.headers);
+      const headersObject = JSON.parse(headersString);
+      discordPayload = await instance.parse(req.body, headersObject);
+    } catch (error) {
+      res.sendStatus(500);
+      logger.error("Error during parse: " + error.stack);
+      discordPayload = ErrorUtil.createErrorPayload(providerName, error);
     }
-    providerInfos.push(providerInfo)
-})
-providerNames.sort()
+  } else {
+    logger.error(`Unknown provider ${providerName}`);
+    res.sendStatus(400);
+  }
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.static('public'))
-app.set('view engine', 'ejs')
-
-app.get('/', (req, res) => {
-    res.render('index', { providers: providerInfos })
-})
-
-app.get('/providers', (req, res) => {
-    res.status(200).send(providerNames)
-})
-
-app.get('/api/webhooks/:webhookID/:webhookSecret/:from', (req, res) => {
-    // Return 200 if the provider is valid to show this url is ready.
-    const provider: any = req.params.from
-    if (provider == null || providersMap.get(provider) == null) {
-        logger.error(`Unknown provider: ${provider}`)
-        res.sendStatus(400)
-    } else {
-        res.sendStatus(200)
-    }
-})
-
-app.post('/api/webhooks/:webhookID/:webhookSecret/:from', async (req, res) => {
-    const webhookID = req.params.webhookID
-    const webhookSecret = req.params.webhookSecret
-    const providerName = req.params.from
-    if (!webhookID || !webhookSecret || !providerName) {
-        res.sendStatus(400)
-        return
-    }
-    const discordEndpoint = 'https://discordapp.com/api/webhooks/' + webhookID + '/' + webhookSecret
-
-    let discordPayload: DiscordPayload = null
-    const error = false
-
-    const Provider = providersMap.get(providerName)
-    if (Provider != null) {
-        const instance: BaseProvider = new Provider()
-        try {
-            // seems dumb, but this is the best way I know how to format these headers in a way we can use them
-            const headersString = JSON.stringify(req.headers)
-            const headersObject = JSON.parse(headersString)
-            discordPayload = await instance.parse(req.body, headersObject)
-        } catch (error) {
-            res.sendStatus(500)
-            logger.error('Error during parse: ' + error.stack)
-            discordPayload = ErrorUtil.createErrorPayload(providerName, error)
-        }
-    } else {
-        logger.error(`Unknown provider ${providerName}`)
-        res.sendStatus(400)
-    }
-
-    if (discordPayload != null) {
-        const jsonString = JSON.stringify(discordPayload)
-        axios({
-            data: jsonString,
-            method: 'post',
-            url: discordEndpoint
-        }).then(() => {
-            res.sendStatus(200)
-        }).catch((err: any) => {
-            logger.error(error)
-            res.sendStatus(400)
-        })
-    }
-})
+  if (discordPayload != null) {
+    const jsonString = JSON.stringify(discordPayload);
+    axios({
+      data: jsonString,
+      method: "post",
+      url: discordEndpoint
+    })
+      .then(() => {
+        res.sendStatus(200);
+      })
+      .catch((err: any) => {
+        logger.error(error);
+        res.sendStatus(400);
+      });
+  }
+});
 
 app.use((req, res, next) => {
-    res.status(404).send('Not Found')
-})
+  res.status(404).send("Not Found");
+});
 
-const port = normalizePort(process.env.PORT || '8080')
+const port = normalizePort(process.env.PORT || "8080");
 
 const server = app.listen(port, () => {
-    logger.debug(`Your app is listening on port ${port}`)
-})
+  logger.debug(`Your app is listening on port ${port}`);
+});
 
 function normalizePort(givenPort: string) {
-    const normalizedPort = parseInt(givenPort, 10)
+  const normalizedPort = parseInt(givenPort, 10);
 
-    if (isNaN(normalizedPort)) {
-        // named pipe
-        return givenPort
-    }
+  if (isNaN(normalizedPort)) {
+    // named pipe
+    return givenPort;
+  }
 
-    if (normalizedPort >= 0) {
-        // port number
-        return normalizedPort
-    }
+  if (normalizedPort >= 0) {
+    // port number
+    return normalizedPort;
+  }
 
-    return false
+  return false;
 }
 
-module.exports = server
+module.exports = server;
